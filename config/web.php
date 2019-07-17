a0dfc924f5f3713226bbac3df413c7e559264073
mal1911 <ma1911@mail.ru>
2019-07-17 23:42:39 +0300

    run yii app

---
+++ config/web.php
@@ -0,0 +1,6 @@
+<?php
+return [
+  'id' => 'school',
+  'basePath' => realpath(__DIR__ . '/../')
+];
+?>
--- web/index.php
+++ web/index.php
@@ -1,2 +1,4 @@
 <?php
-echo "hello";
+require __DIR__ . '/../vendor/yiisoft/yii2/yii.php';
+$config = require __DIR__ . '/../config/web.php';
+(new yii\web\Application($config))->run();
